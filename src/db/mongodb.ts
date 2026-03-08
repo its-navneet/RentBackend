b40a990923994712afa708cdc82ca1a58eb499f7
Navneet Gupta <navneet@192.168.1.7>
2026-03-08 11:11:52 +0530

    upd: mongo

--- src/db/mongodb.ts
+++ src/db/mongodb.ts
@@ -3,16 +3,17 @@
  * Handles connection to MongoDB database
  */
 
-import mongoose from 'mongoose';
-import { log } from 'console';
+import mongoose from "mongoose";
+import { log } from "console";
 
-const MONGODB_URI = "mongodb+srv://admin:navneet@cluster0.juvuasp.mongodb.net/?appName=Cluster0"
+const MONGODB_URI = process.env.MONGODB_URI || "";
 
 export const connectDB = async (): Promise<void> => {
-
   try {
     console.log("mongo_url", MONGODB_URI);
-    const conn = await mongoose.connect(MONGODB_URI);
+    const conn = await mongoose.connect(MONGODB_URI, {
+      tls: true,
+    });
     log(`MongoDB Connected: ${conn.connection.host}`);
   } catch (error: any) {
     log(`Error connecting to MongoDB: ${error.message}`);
@@ -22,13 +23,12 @@ export const connectDB = async (): Promise<void> => {
 };
 
 // Handle MongoDB connection events
-mongoose.connection.on('disconnected', () => {
-  log('MongoDB disconnected');
+mongoose.connection.on("disconnected", () => {
+  log("MongoDB disconnected");
 });
 
-mongoose.connection.on('error', (err) => {
+mongoose.connection.on("error", (err) => {
   log(`MongoDB error: ${err.message}`);
 });
 
 export default mongoose;
-
